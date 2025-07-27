059fae4d2aa7698153752fc2853d9f566bdad0f6
nafisshahriar98 <nafis.shahriar045@gmail.com>
2025-07-27 15:41:31 +0600

    project.blade.php updated in property cards section

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -114,111 +114,86 @@
                       </button>
                     </div>
                   </form>
-                  @foreach($projects as $project)
+                 
                   <!-- property cards -->
-                  <div class="-mx-15px">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-                      <!-- card 1 -->
-                      <div class="apart-card mb-50px px-15px">
-                        <div
-                          class="group border border-border-color-13 shadow-box-shadow-4"
-                        >
-                          <!-- card thumbs -->
-                          <div class="relative leading-1">
-                            <a
-                              href="product-details.html"
-                              class="overflow-hidden"
-                            >
-                              <img
-                                src="{{asset('storage/'.$project->image1 ?? '')}}"
-                                class="w-full group-hover:scale-110 transition-all duration-700"
-                                alt=""
-                              >
-                            </a>
-                          </div>
-                          <!-- card body -->
-                          <div class="px-5 pt-30px lg:px-30px">
-                            <p
-                              class="text-secondary-color font-semibold mb-15px uppercase"
-                            >
-                              <span class="leading-25px">{{$project->place}}</span>
-                            </p>
-                            <h4
-                              class="text-lg md:text-xl lg:text-22px font-semibold text-heading-color mb-15px"
-                            >
-                              <a
-                                href="{{route('project.show', $project->slug)}}"
-                                class="hover:text-secondary-color leading-1.3"
-                                >{{$project->title}}</a
-                              >
-                            </h4>
+            <div class="-mx-15px">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    @foreach($projects as $project)
+      <!-- card 1 -->
+      <div class="apart-card mb-50px px-15px">
+        <div class="group border border-border-color-13 shadow-box-shadow-4">
+          <!-- card thumbs -->
+          <div class="relative leading-1">
+            <a href="product-details.html" class="overflow-hidden">
+              <img
+                src="{{ asset('storage/'.$project->image1 ?? '') }}"
+                class="w-full group-hover:scale-110 transition-all duration-700"
+                alt=""
+              >
+            </a>
+          </div>
 
-                            <div class="text-sm">
-                              <a
-                                href="{{route('project.show', $project->slug)}}"
-                                class="hover:text-secondary-color"
-                                ><i
-                                  class="flaticon-pin text-secondary-color mr-0.5"
-                                ></i>
-                                {{$project->address}}</a
-                              >
-                            </div>
+          <!-- card body -->
+          <div class="px-5 pt-30px lg:px-30px">
+            <p class="text-secondary-color font-semibold mb-15px uppercase">
+              <span class="leading-25px">{{ $project->place }}</span>
+            </p>
+            <h4 class="text-lg md:text-xl lg:text-22px font-semibold text-heading-color mb-15px">
+              <a href="{{ route('project.show', $project->slug) }}" class="hover:text-secondary-color leading-1.3">
+                {{ $project->title }}
+              </a>
+            </h4>
+            <div class="text-sm">
+              <a href="{{ route('project.show', $project->slug) }}" class="hover:text-secondary-color">
+                <i class="flaticon-pin text-secondary-color mr-0.5"></i> {{ $project->address }}
+              </a>
+            </div>
 
-                            <ul class="flex flex-wrap gap-15px pt-4 pb-5">
+            <ul class="flex flex-wrap gap-15px pt-4 pb-5">
+              <li>
+                <p class="leading-1.8 font-bold text-sm">
+                  {{ $project->squareft }} <span class="font-normal">Square Ft</span>
+                </p>
+              </li>
+            </ul>
 
-                              <li>
-                                <p class="leading-1.8 font-bold text-sm">
-                                {{$project->squareft}}
-                                  <span class="font-normal">Square Ft</span>
-                                </p>
-                              </li>
-                            </ul>
-                            <div
-                              class="card-quick-area flex gap-x-2 pb-30px leading-1 transition-all duration-300"
-                            >
-                              <ul class="flex gap-x-2">
-                                <li>
-                                  <button
-                                    data-modal-index="1"
-                                    class="modal-open w-9 h-9 flex items-center justify-center bg-section-bg-1 text-center hover:bg-secondary-color hover:text-white font-bold"
-                                  >
-                                    <i class="flaticon-expand leading-1"></i>
-                                  </button>
-                                </li>
-                                <li>
-                                  <button
-                                    data-modal-index="2"
-                                    class="modal-open w-9 h-9 flex items-center justify-center bg-section-bg-1 text-center hover:bg-secondary-color hover:text-white font-bold"
-                                  >
-                                    <i class="flaticon-heart-1 leading-1"></i>
-                                  </button>
-                                </li>
-                                <li>
-                                  <a
-                                    href="{{route('project.show', $project->slug)}}"
-                                    class="w-9 h-9 flex items-center justify-center bg-section-bg-1 text-center hover:bg-secondary-color hover:text-white font-bold"
-                                    ><i class="flaticon-add leading-1"></i
-                                  ></a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                          <!-- cart footer -->
-                          <div
-                            class="p-5 lg:px-30px border-t border-border-color-1"
-                          >
-                            <h5
-                              class="text-lg text-secondary-color font-semibold mb-5px"
-                            >
-                              <span class="leading-1.8">{{$project->price}}</span>
-                              <label class="text-sm font-normal">/Month</label>
-                            </h5>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  @endforeach
+            <div class="card-quick-area flex gap-x-2 pb-30px leading-1 transition-all duration-300">
+              <ul class="flex gap-x-2">
+                <li>
+                  <button data-modal-index="1"
+                          class="modal-open w-9 h-9 flex items-center justify-center bg-section-bg-1 hover:bg-secondary-color hover:text-white font-bold">
+                    <i class="flaticon-expand leading-1"></i>
+                  </button>
+                </li>
+                <li>
+                  <button data-modal-index="2"
+                          class="modal-open w-9 h-9 flex items-center justify-center bg-section-bg-1 hover:bg-secondary-color hover:text-white font-bold">
+                    <i class="flaticon-heart-1 leading-1"></i>
+                  </button>
+                </li>
+                <li>
+                  <a href="{{ route('project.show', $project->slug) }}"
+                     class="w-9 h-9 flex items-center justify-center bg-section-bg-1 hover:bg-secondary-color hover:text-white font-bold">
+                    <i class="flaticon-add leading-1"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <!-- cart footer -->
+          <div class="p-5 lg:px-30px border-t border-border-color-1">
+            <h5 class="text-lg text-secondary-color font-semibold mb-5px">
+              <span class="leading-1.8">{{ $project->price }}</span>
+              <label class="text-sm font-normal">/Month</label>
+            </h5>
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+</div>
+
                 </div>
                 <!-- content 2 -->
                 <div class="hidden">
